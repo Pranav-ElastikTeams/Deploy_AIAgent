e4e770824a3d2c244b484d8e27a9a1166f1e42a8
Pranav9211 <pranavbharti9211@gmail.com>
2025-06-25 22:51:06 +0530

    second deployment

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def complaint_types():
 #     app.run(debug=True) 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
